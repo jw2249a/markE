cb17014cf5d0ddeb80bdd69f7d755cd08d5e7530
J R Williams <jackryanwilliams@gmail.com>
2017-11-26 16:27:45 -0500

    finally working on this

--- src/markE.c
+++ src/markE.c
@@ -1,7 +1,23 @@
 #include <stdio.h> 
 
-int main()
+int main(int argc, char *argv[])
 {
-  
+  printf("there were %d commands\n", argc);
+  while (*argv != '\0')
+    {
+      printf("%s \n", *argv);
+      argv++;
+    }
   return 0;
+}
+
+
+void create_project(char *argv)
+{
+}
+void create_app(char *argv)
+{
+}
+void branch(char *argv)
+{
 }
