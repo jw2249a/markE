a48f205da42f51ffd7e7378415da7bbd9532bee7
J R Williams <jackryanwilliams@gmail.com>
2017-11-27 00:38:16 -0500

    adding db stuff

--- src/markE.c
+++ src/markE.c
@@ -1,23 +1,55 @@
 #include <stdio.h> 
+#include <db.h>
+
 
 int main(int argc, char *argv[])
 {
+  DBT key, data;
+  DB *dbp;
+  u_int32_t flags;
+  int ret;
+  char database[] =  __FILE__".db";
+  
   printf("there were %d commands\n", argc);
   while (*argv != '\0')
     {
       printf("%s \n", *argv);
       argv++;
     }
+  ret = db_create(&dbp, NULL, 0);
+  flags = DB_CREATE;
+  if (ret != 0)
+    {
+      /* error handling goes here*/
+    }
+  ret = dbp->open(dbp,NULL,database, NULL, DB_BTREE, flags, 0);
+  if (ret != 0)
+    {
+      /* error handling goes here*/
+    }
+  if (dbp != NULL)
+    {
+      dbp->close(dbp,0);
+    }
   return 0;
 }
 
 
-void create_project(char *argv)
-{
-}
-void create_app(char *argv)
-{
-}
+void init(char *argv)
+{}
+// initialize a project
+void create(char *argv)
+{}
+// options to create a app or a static from here
+void run(void)
+{}
+// run server from application or static dependending on location
+void build()
+{}
+// build a project so it is ready to deploy (compiles into binaries)
+void merge()
+{}
+// merge branched apps or statics
 void branch(char *argv)
-{
-}
+{}
+//branch an app or static 
