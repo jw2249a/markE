33d82ac9f87ce5b5106e2c2f339b4e8899a753f2
J R Williams <jw2249a@student.american.edu>
2018-01-05 11:18:06 -0500

    not making this mistake again

--- include/markE.h
+++ include/markE.h
@@ -1,9 +1,5 @@
 #ifndef MARKE_H
 #define MARKE_H
 /* function prototypes */
-void create(char *argv); // options to create a app or a static from here
-void run(void); // run server from application or static dependending on location
-void build(void);// build a project so it is ready to deploy (compiles into binaries)
-void merge(void); // merge branched apps or statics
-void branch(char *argv); //branch an app or static
+
 #endif 
